3dc309920411148941626155598bd3dd05c76c94
Luke McEwen <lukemhockey9@gmail.com>
2025-02-21 23:46:45 -0500

    scaffolding for discordbot

--- cmd/main.go
+++ cmd/main.go
@@ -209,6 +209,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ShopifyScraper")
 		os.Exit(1)
 	}
+	if err = (&controller.DiscordBotReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DiscordBot")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
